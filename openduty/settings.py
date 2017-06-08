0171d0f7d2f488e383d2d38eec1ecddc48a1ff99
gu3sss <gu3sss@users.noreply.github.com>
2017-06-08 14:32:08 -0400

    Create settings.py

--- openduty/settings.py
+++ openduty/settings.py
@@ -123,8 +123,8 @@ EMAIL_SETTINGS = {
 }
 
 TWILIO_SETTINGS = {
-    'SID': "SKcd2af9e31ba9c2018177fa6e9828a4d5",
-    'token': "SAPa3OYMYu6cDvhu7zePqJO9P4IO7lRM",
+    'SID': "ACa388ccebaea03bb36f9b1ef1de1617af",
+    'token': "869f7d0044468a5aab446b24f98cd949",
     'phone_number': "+1 210-908-5879 ",
     'sms_number': "+1 210-908-5879 ",
     'twiml_url': "https://github.com/gu3sss/openduty/blob/master/extra/voice.xml"
