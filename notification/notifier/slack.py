19dc1cf00005aa83fb0e212b75cf90eee78d0456
gu3sss <gu3sss@users.noreply.github.com>
2017-06-22 13:39:01 -0400

    Update slack.py

--- notification/notifier/slack.py
+++ notification/notifier/slack.py
@@ -6,7 +6,7 @@ class SlackNotifier:
         self.__config = config
     
     def notify(self, notification):
-        slack = Slacker(self.__config['apikey'])
+        slack = Slacker(self.__config['xoxp-202075823266-202075823458-202078565794-22ee9b3e42b2621e76911e68ef44cb7b'])
         response = slack.chat.post_message(notification.user_to_notify.profile.slack_room_name, notification.message,
                                            username="Openduty", icon_url="https://slack.global.ssl.fastly.net/1937/img/services/pagerduty_48.png")
         if not response.error:
