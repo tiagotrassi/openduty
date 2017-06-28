cfc55dea4875205968dbfb7e77d416c7ee0c9b2c
gu3sss <gu3sss@users.noreply.github.com>
2017-06-28 15:19:42 -0400

    Update incidents.py

--- openduty/incidents.py
+++ openduty/incidents.py
@@ -221,9 +221,9 @@ def update_type(request):
 	if incident.event_type == "trigger":
         	if incident_ids:
 			_update_type(request.user, incident_ids, event_type)
-        else:
-        	id = request.POST.get('id')
-		_update_type(request.user, [id], event_type)
+       		else:
+        		id = request.POST.get('id')
+			_update_type(request.user, [id], event_type)
               
     except Incident.DoesNotExist:
         messages.error(request, 'Incident not found')
