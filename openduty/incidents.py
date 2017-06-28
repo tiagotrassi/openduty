db3988773566a30e272c035b767150944fd9c444
gu3sss <gu3sss@users.noreply.github.com>
2017-06-28 15:18:48 -0400

    Update incidents.py

--- openduty/incidents.py
+++ openduty/incidents.py
@@ -218,13 +218,12 @@ def update_type(request):
         messages.error(request, 'Invalid event modification!')
         return HttpResponseRedirect(request.POST['url'])
     try:
-        if incident_ids:
-			if incident.event_type == "trigger":
-				_update_type(request.user, incident_ids, event_type)
+	if incident.event_type == "trigger":
+        	if incident_ids:
+			_update_type(request.user, incident_ids, event_type)
         else:
         	id = request.POST.get('id')
-		if incident.event_type == "trigger":
-			_update_type(request.user, [id], event_type)
+		_update_type(request.user, [id], event_type)
               
     except Incident.DoesNotExist:
         messages.error(request, 'Incident not found')
